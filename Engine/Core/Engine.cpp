f44291cfa97fa483c0fc2b1eb0011afeb115ba39
KubaNowy12 <JakubNowacki2012@gmail.com>
2025-06-03 14:18:54 +0200

    Finished running the stand-alone game.

--- Engine/Core/Engine.cpp
+++ Engine/Core/Engine.cpp
@@ -1,14 +1,22 @@
+#if !defined(SHIPPING)
+#include "..\Content\ContentLoader.h"
+#include "..\Components\Script.h"
+#include <thread>
+
 bool engine_initialize()
 {
-	return true;
+	bool result{ easepix::content::load_game() };
+	return result;
 }
 
 void engine_update()
 {
-
+	easepix::script::update(10.f);
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
 
 void engine_shutdown()
 {
-
+	easepix::content::unload_game();
 }
+#endif
