fca422f9397ac221927c351db1e731c0f8f996c9
KubaNowy12 <JakubNowacki2012@gmail.com>
2025-07-03 18:34:32 +0200

    Added a swap chain and using the render surface.

--- Engine/Graphics/Direct3D12/D3D12Core.cpp
+++ Engine/Graphics/Direct3D12/D3D12Core.cpp
@@ -1,5 +1,6 @@
 #include "D3D12Core.h"
 #include "D3D12Resources.h"
+#include "D3D12Surface.h"
 
 using namespace Microsoft::WRL;
 
@@ -156,6 +157,7 @@ private:
 ID3D12Device14*				main_device{ nullptr };
 IDXGIFactory7*				dxgi_factory{ nullptr };
 d3d12_command				gfx_command;
+utl::vector<d3d12_surface>	surfaces;
 
 descriptor_heap				rtv_desc_heap{ D3D12_DESCRIPTOR_HEAP_TYPE_RTV };
 descriptor_heap				dsv_desc_heap{ D3D12_DESCRIPTOR_HEAP_TYPE_DSV };
@@ -164,8 +166,9 @@ descriptor_heap				uav_desc_heap{ D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV };
 
 utl::vector<IUnknown*>		deferred_releases[frame_buffer_count]{};
 u32							deferred_releases_flag[frame_buffer_count]{};
-std::mutex					deferred_releases_mutx{};
+std::mutex					deferred_releases_mutex{};
 
+constexpr DXGI_FORMAT render_target_format{ DXGI_FORMAT_R8G8B8A8_UNORM_SRGB };
 constexpr D3D_FEATURE_LEVEL minimum_feature_level{ D3D_FEATURE_LEVEL_11_0 };
 
 bool
@@ -218,7 +221,7 @@ get_max_feature_level(IDXGIAdapter4* adapter)
 void __declspec(noinline)
 process_deferred_releases(u32 frame_idx)
 {
-	std::lock_guard lock{ deferred_releases_mutx };
+	std::lock_guard lock{ deferred_releases_mutex };
 
 	deferred_releases_flag[frame_idx] = 0;
 
@@ -241,7 +244,7 @@ namespace detail {
 void deferred_release(IUnknown* resource)
 {
 	const u32 frame_idx{ current_frame_index() };
-	std::lock_guard lock{ deferred_releases_mutx };
+	std::lock_guard lock{ deferred_releases_mutex };
 	deferred_releases[frame_idx].push_back(resource);
 	set_deferred_releases_flag();
 }
@@ -355,8 +358,67 @@ shutdown()
 	release(main_device);
 }
 
+ID3D12Device *const
+device() { return main_device; }
+
+descriptor_heap&
+rtv_heap() { return rtv_desc_heap; }
+
+descriptor_heap&
+dsv_heap() { return dsv_desc_heap; }
+
+descriptor_heap&
+srv_heap() { return srv_desc_heap; }
+
+descriptor_heap&
+uav_heap() { return uav_desc_heap; }
+
+DXGI_FORMAT
+default_render_target_format() { return render_target_format; }
+
+u32
+current_frame_index() { return gfx_command.frame_index(); }
+
+void set_deferred_releases_flag() { deferred_releases_flag[current_frame_index()] = 1; }
+
+surface
+create_surface(platform::window window)
+{
+	surfaces.emplace_back(window);
+	surface_id id{ (u32)surfaces.size() - 1 };
+	surfaces[id].create_swap_chain(dxgi_factory, gfx_command.command_queue(), render_target_format);
+	return surface{ id };
+}
+
 void
-render()
+remove_surface(surface_id id)
+{
+	gfx_command.flush();
+	surfaces[id].~d3d12_surface();
+	//surfaces[id] = d3d12_surface{};
+}
+
+void
+resize_surface(surface_id id, u32, u32)
+{
+	gfx_command.flush();
+	surfaces[id].resize();
+}
+
+u32
+surface_width(surface_id id)
+{
+	return surfaces[id].width();
+}
+
+u32
+surface_height(surface_id id)
+{
+	return surfaces[id].height();
+}
+
+void
+render_surface(surface_id id)
 {
 	gfx_command.begin_frame();
 	ID3D12GraphicsCommandList6* cmd_list{ gfx_command.command_list() };
@@ -367,15 +429,11 @@ render()
 		process_deferred_releases(frame_idx);
 	}
 
+	const d3d12_surface& surface{ surfaces[id] };
+
+	surface.present();
+
 	gfx_command.end_frame();
 }
-
-ID3D12Device *const
-device() { return main_device; }
-
-u32
-current_frame_index() { return gfx_command.frame_index(); }
-
-void set_deferred_releases_flag() { deferred_releases_flag[current_frame_index()] = 1; }
 
 }
